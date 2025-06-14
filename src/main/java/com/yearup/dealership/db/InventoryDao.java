950dceb24f7a3f91c34c73917b492bb923879cd0
Amena Nazari <nazara@jevs.org>
2025-06-14 07:51:30 -0400

    add Vehicle To Inventory

--- src/main/java/com/yearup/dealership/db/InventoryDao.java
+++ src/main/java/com/yearup/dealership/db/InventoryDao.java
@@ -12,9 +12,22 @@ public class InventoryDao {
         this.dataSource = dataSource;
     }
 
-    public void addVehicleToInventory(String vin, int dealershipId) {
-        // TODO: Implement the logic to add a vehicle to the inventory
+    public void addVehicleToInventory(String vin, int dealershipId) {String sql = "INSERT INTO inventory (vin, dealership_id) VALUES (?, ?)";
+
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, vin);
+            stmt.setInt(2, dealershipId);
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+        // TODO: Implement the logic to add a vehicle to the inventory
+
 
     public void removeVehicleFromInventory(String vin) {
         // TODO: Implement the logic to remove a vehicle from the inventory
